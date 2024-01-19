0ef96685827217c35fa22d3525817a5a1090e024
Parkseojin2001 <seojin0510612@gmail.com>
2024-01-19 21:57:09 +0900

    Update Router.js
    #4.6 ~ #4.8

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { edit } from "../controllers/userController";
-import { remove
- } from "../controllers/userController";
+import { edit, remove, see, logout } from "../controllers/userController";
+
 const userRouter = express.Router();
 
+userRouter.get("/:id", see);
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 
