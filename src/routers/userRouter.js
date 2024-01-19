aaae6e0fd34e8cf2812303d6c684f8e3dfd81949
Parkseojin2001 <seojin0510612@gmail.com>
2024-01-19 20:30:13 +0900

    Update to clean code
    #4.0 ~ #4.3

---
+++ src/routers/userRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { edit } from "../controllers/userController";
+import { remove
+ } from "../controllers/userController";
+const userRouter = express.Router();
+
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
+
+export default userRouter;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const POST = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const home = (req, res) => {
-  return res.send("hello");
-};
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+
 
 const handleListening = () =>
   console.log(`✅Sever listening on port http://localhost:${POST}  🚀`);
